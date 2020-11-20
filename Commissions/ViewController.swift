714efb0c871e6db8be9ddb1c63655b234c23c8f5
natalie <njurcik4503@stu.d214.org>
2020-11-19 20:10:46 -0600

    added labels and more

--- Commissions/ViewController.swift
+++ Commissions/ViewController.swift
@@ -6,15 +6,21 @@
 //  Copyright © 2020 Natalie Jurcik. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalPayLabel: UILabel!
+    @IBOutlet weak var commissionPayTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenCalculateButtonPressed(_ sender: Any) {
+    }
+    
 }
 
